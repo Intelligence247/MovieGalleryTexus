05b898b9b9bee03339540110547d0dcec07667ce
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-03-12 21:11:23 +0100

    fixes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div>
+      <img src="/media/img1.png" alt="" />
+      <div className="information">
+    <div className="title">
+      <h1></h1>
+    </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Main.css"
 const Main = () => {
   return (
     <main>
-        
+
     </main>
   )
 }
