e6f358ade6e0b51f8dafe4a373ea3aa7e9186e2b
piouskenny <kehindeu13@gmail.com>
2023-03-15 10:08:32 +0100

    fixed deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 function App() {
-  return(
-      <div className="body">
-
-     <Header/>
-    <Main/>
-
-  </div>
+  return (
+    <div className="body">
+      <Header />
+      <Main />
+    </div>
   );
 }
 
 export default App;
-
 
 // https://www.figma.com/file/jhFRdFIdHpRxsDznNXtpXw/Watchlist-Creator?node-id=2%3A17
